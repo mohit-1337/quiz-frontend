4b146d124e64f92b0feafbb22231db74b77067c1
MohitD9 <deshpandemohit1999@gmail.com>
2024-08-21 16:27:34 +0530

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-heading">Welcome To QUIZZIE</div>
+    </div>
+  );
+};
+
+export default Home;
